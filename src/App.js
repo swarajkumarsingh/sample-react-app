97001f1e2605a468bde5261825bc09fe0186a96b
Swaraj Kumar Singh <89764448+swarajkumarsingh@users.noreply.github.com>
2024-12-24 18:09:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Upated NOW NOW SIMA IS THE BEST
         </a>
       </header>
     </div>
